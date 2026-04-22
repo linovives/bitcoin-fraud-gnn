12c6a90eef961a336b5789bd388ae78a2fdf0d89
LinoVives16 <linovivescolon@gmail.com>
2026-04-22 10:17:35 +0200

    src: add reusable GCN and GAT model definitions

---
+++ src/models.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, GATConv
+
+
+class GCN(torch.nn.Module):
+    """3-layer Graph Convolutional Network for node classification."""
+
+    def __init__(self, in_channels, hidden_channels, out_channels, dropout=0.5):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, out_channels)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv3(x, edge_index)
+        return x
+
+
+class GAT(torch.nn.Module):
+    """3-layer Graph Attention Network for node classification."""
+
+    def __init__(self, in_channels, hidden_channels, out_channels, heads=4, dropout=0.5):
+        super().__init__()
+        self.conv1 = GATConv(in_channels, hidden_channels, heads=heads, dropout=dropout)
+        self.conv2 = GATConv(hidden_channels * heads, hidden_channels, heads=heads, dropout=dropout)
+        self.conv3 = GATConv(hidden_channels * heads, out_channels, heads=1, concat=False, dropout=dropout)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index):
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        x = F.elu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv3(x, edge_index)
+        return x
